8f57607a7d56345dad4582aaaf3ad3cf6b9b164f
Carlos Barajas <soldotsol@gmail.com>
2023-11-02 21:13:35 -0700

    Initial project setup

---
+++ src/main/java/org/solthoth/java/implementing_dao/JDBCExecutor.java
@@ -0,0 +1,7 @@
+package org.solthoth.java.implementing_dao;
+
+public class JDBCExecutor {
+    public static void main (String... args) {
+        System.out.println("Hello Learning JDBC");
+    }
+}
